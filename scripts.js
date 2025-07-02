6c3d7ea6507bfa15e3fbd643f6629395443071ca
kathlyn <kathlyn@okngroup.com.br>
2025-07-02 15:42:12 -0300

    Par ou Impar

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Par ou Impar?
+// Destravando o poder do JavaScript com este exercício simples!
+
+exibirResultado(5);
+function exibirResultado(numero) {
+    for (let i = onabort; i < numero; i++) {
+        if (i % 2 === 0)
+            console.log(i + "Par");
+        else
+            console.log(i + "Impar");
+
+    }
+}
